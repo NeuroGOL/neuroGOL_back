00ec800864361584c22f71e7ed10aefee8da5b06
Juan Rodríguez <juamcamirodri@gmail.com>
2025-08-10 13:58:22 -0500

    ok

--- ola.py
+++
@@ -1,4 +0,0 @@
-from transformers import AutoTokenizer
-tokenizer = AutoTokenizer.from_pretrained("bert-base-uncased")
-tokens = tokenizer(["test", "another"], padding=True, truncation=True, return_tensors="pt")
-print(tokens)

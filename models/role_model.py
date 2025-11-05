172de6716ff528e51e3da772611fb50dece10503
Juan Rodríguez <juamcamirodri@gmail.com>
2025-11-05 14:44:10 -0500

    ya

--- models/role_model.py
+++ models/role_model.py
@@ -3,7 +3,7 @@ from extensions import db
 class Role(db.Model):
     __tablename__ = "roles"
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(50), nullable=False)
 
     def to_dict(self):

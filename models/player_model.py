8d8d2736c51603afa96c7344bea6b809f6fa9a30
Juan Rodríguez <juamcamirodri@gmail.com>
2025-11-05 14:23:51 -0500

    faltan graficas

--- models/player_model.py
+++ models/player_model.py
@@ -4,7 +4,7 @@ from datetime import datetime
 class Player(db.Model):
     __tablename__ = 'players'
 
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     nombre = db.Column(db.String(100), nullable=False)
     equipo = db.Column(db.String(100), nullable=False)
     nacionalidad = db.Column(db.String(100), nullable=False)
